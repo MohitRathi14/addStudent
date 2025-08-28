fc86662056f0d9af34e117ddb67840b427cc8ae2
MohitRathi14 <aaditya375singh2@gmail.com>
2025-08-28 12:34:36 +0530

    Initialize admin setup in the application

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const user = require('./routes/user');
 const path = require('path');
 const connectDB = require('./connection.js');
+const makeAdmin = require('./makeadmin');
 const student = require('./routes/student');
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 connectDB();
+makeAdmin();
 app.use(user)
 app.use(student);
 app.set('view engine', 'ejs');
